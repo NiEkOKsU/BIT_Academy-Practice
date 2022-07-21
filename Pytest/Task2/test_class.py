e60b9de793912185dc57d597f50eb868a73da828
NiEkOKsU <bartek.kozera00@gmail.com>
2022-07-21 14:54:20 +0200

    Task2 done

--- Pytest/Task2/test_class.py
+++ Pytest/Task2/test_class.py
@@ -1,15 +1,100 @@
+import pytest
 from function import is_prime
+from math import sqrt
+from random import randint
+#Złożoność: O(logx)
+def prime_num(x):
+
+    if x < 2:
+        return False
+
+    if x == 2 or x == 3:
+        return True
+
+    if x % 2 == 0 or x % 3 == 0:
+        return False
+
+    tmp = 5
+
+    while int(sqrt(x)) + 1 > tmp:
+
+        if x % tmp == 0:
+            return False
+
+        tmp += 2
+
+        if x % tmp == 0:
+            return False
+        
+        tmp += 4
+    
+    return True
+
 
 class TestMini:
-    # Napisać testy sprawdzające funkcję is_prime dla liczb
-    # z przedziału <-1, 6>
+
+    def testa(self):
+        assert is_prime(-1) == False
+
+    def testb(self):
+        assert is_prime(0) == False
+
+    def testc(self):
+        assert is_prime(1) == False
+
+    def testd(self):
+        assert is_prime(2) == True
+
+    def teste(self):
+        assert is_prime(3) == True
+
+    def testf(self):
+        assert is_prime(4) == False
+
+    def testg(self):
+        assert is_prime(5) == True
+
+    def testh(self):
+        assert is_prime(6) == False
 
 class TestMid:
-    # Napisać 5 testów sprawdzających funkcję is_prime dla losowych
-    # liczb z przedziału <10 ** 4, 10 ** 5>
+    def testa(self):
+        x = randint(10**4, 10**5)
+        assert is_prime(x) == prime_num(x)
+
+    def testb(self):
+        x = randint(10**4, 10**5)
+        assert is_prime(x) == prime_num(x)
+
+    def testc(self):
+        x = randint(10**4, 10**5)
+        assert is_prime(x) == prime_num(x)
+
+    def testd(self):
+        x = randint(10**4, 10**5)
+        assert is_prime(x) == prime_num(x)
+
+    def teste(self):
+        x = randint(10**4, 10**5)
+        assert is_prime(x) == prime_num(x)
 
 class TestMaxi:
-    # Napisać 5 testów sprawdzających funkcję is_prime dla losowych
-    # z przedziału <10 ** 12, 10 ** 13>, a następnie
-    # naprawić funkcję is_prime aby testy wykonywały się szybciej
-    # niż dla aktualnej funkcji
+    def testa(self):
+        x = randint(10**12, 10**13)
+        assert is_prime(x) == prime_num(x)
+
+    def testb(self):
+        x = randint(10**12, 10**13)
+        assert is_prime(x) == prime_num(x)
+
+    def testc(self):
+        x = randint(10**12, 10**13)
+        assert is_prime(x) == prime_num(x)
+
+    def testd(self):
+        x = randint(10**12, 10**13)
+        assert is_prime(x) == prime_num(x)
+
+    def teste(self):
+        x = randint(10**12, 10**13)
+        assert is_prime(x) == prime_num(x)
