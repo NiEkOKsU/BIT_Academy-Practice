3d6e1f8778390272fe857f9eee2765854c600a8f
NiEkOKsU <bartek.kozera00@gmail.com>
2022-07-30 17:10:52 +0200

    Inicjalizacja, status robota

--- OOP/Task1/robot.py
+++ OOP/Task1/robot.py
@@ -8,24 +8,48 @@ BATTERY_VAL = 10
 class robot:
     # mapa, x, y, bateria
     def __init__(self, T, x, y, b):
-        pass
+        self.__T = T
+        self.__x = x
+        self.__y = y
+        self.__b = b
 
     def left(self, val = 1):
-        pass
+        return self
     def right(self, val = 1):
-        pass
+        return self
     def up(self, val = 1):
-        pass
+        return self
     def down(self, val = 1):
-        pass
+        return self
 
     def get_status(self):
-        pass
+        if self.get_battery == 0:
+            return robotStatus.DEAD
+
+        T, x, y = self.__T, self.get_x(), self.get_y()
+
+        if T[x][y] == 'T':
+            return robotStatus.ALIVE
+        elif T[x][y] == 'W':
+            return robotStatus.WATER
+        return robotStatus.CRASH
+
     def get_battery(self):
-        pass
+        return self.__b
+
     def get_map(self):
-        pass
+        status = self.get_status()
+        x, y = self.get_x(), self.get_y()
+        T = self.__T
+
+        if status == robotStatus.ALIVE:
+            T[x][y] = 'R'
+            return T
+        T[x][y] = 'x'
+        return T
+
     def get_x(self):
-        pass
+        return self.__x
+
     def get_y(self):
-        pass
+        return self.__y
